a7d1a7685a3f0a650499978bd6f945f64822e713
Teerayut627 <31033162+Teerayut627@users.noreply.github.com>
2018-11-18 22:31:25 +0700

    No nand + convert binary str to int dec

--- comArchProj.py
+++ comArchProj.py
@@ -37,6 +37,35 @@ def bindigits(n, bits):
     s = bin(n & int("1"*bits, 2))[2:]
     return ("{0:0>%s}" % (bits)).format(s)
 
+def sign_bit (imm) : 
+    result = ''
+    imm = str(imm)
+    for i in imm :
+        if i == '1':
+            result = result + "0"
+        else :
+            result = result + "1"            
+    return add_binary_nums(result[0:-1], bin(1))
+    
+def add_binary_nums(x, y) :
+        max_len = max(len(x), len(y))
+
+        x = x.zfill(max_len)
+        y = y.zfill(max_len)
+
+        result = ''
+        carry = 0
+
+        for i in range(max_len-1, -1, -1) :
+            r = carry
+            r += 1 if x[i] == '1' else 0
+            r += 1 if y[i] == '1' else 0
+            result = ('1' if r % 2 == 1 else '0') + result
+            carry = 0 if r < 2 else 1       
+
+        if carry !=0 : result = '1' + result
+        return result.zfill(max_len)
+
 with open('D:/CPE/ComArch/test.txt') as f:
     lines = f.readlines()
     line_label = []
@@ -100,11 +129,11 @@ with open('D:/CPE/ComArch/test.txt') as f:
             if(find == True):
                 z = count
                 print(int(z))
-                machine = bindigits(int(z),32)
+                machine = str(int(z))
                 
             else:
                 print(int(x))
-                machine = bindigits(int(x),32)
+                machine = str(int(x))
               
     #beq
         elif(instr[1] == "beq"):
@@ -150,63 +179,65 @@ with open('D:/CPE/ComArch/machineCode.txt') as f:
 
 
 memline = 0
-while(memline < 2):
+while(memline < len(mem)):
     opcode = mem[int(memline)][7:10]
+
+    print("@@@")
+    print("state :")
+    print("     PC : " + str(memline))
+    print("     memory :")
+    i = 0
+    while i < (len(mem)):
+        print("     mem[ " + str(i) +  " ] " + mem[i])
+        i = i + 1
+
+    print("register :")
+    j = 0
+    while j < (len(reg)):
+        print("     reg[ " + str(j) +  " ] " + str(reg[j]))
+        j = j + 1
 
     if (opcode == "010"):
         rs = mem[int(memline)][10:13]
         rt = mem[int(memline)][13:16]
         offset = mem[int(memline)][16:32]
-        print("opcode = "+opcode +" "+"rs = " +rs +" "+ "rt = " +rt +" "+ "offset = " +offset)
-        
-        # addr = int(mem[int(offset,2)],2) 
         addr = int(offset,2)
         valueInRs = reg[int(rs,2)]
         rtAddr = int(rt,2)
         addrValue = addr + int(valueInRs)
-        reg[rtAddr] = int(mem[addrValue],2)
+        reg[rtAddr] = mem[addrValue]
+    if (opcode == "011"):
+        rs = mem[int(memline)][10:13]
+        rt = mem[int(memline)][13:16]
+        offset = mem[int(memline)][16:32]
+        valueInRs = reg[int(rs,2)]
+        addr = int(offset,2)
+        addrValue = addr + int(valueInRs)
+        mem[addrValue] = reg[int(rt)]
+    if (opcode == "100"): #BEQ
+        rs = mem[int(memline)][10:13]
+        rt = mem[int(memline)][13:16]
+        offset = mem[int(memline)][16:32]
+        if( reg[int(rt)] == reg[int(rs)]):
+            # print(memline)
+            if(mem[memline][16:17] == "1"):
+                offsetJamp = int(add_binary_nums(sign_bit(offset),"1"),2)*(-1)
+            else:
+                offsetJamp = int(offset,2)
+            jampAddr = memline + offsetJamp
+            memline = jampAddr
+            # print(offsetJamp)
+            # print(jampAddr)
+    if (opcode == "000"):
+        rs = mem[int(memline)][10:13]
+        rt = mem[int(memline)][13:16]
+        destReg = mem[int(memline)][29:32]
+        addResult = int(reg[int(rt,2)]) + int(reg[int(rs,2)]) 
+        reg[int(destReg,2)] = addResult
 
+    if (opcode == "110"):
+        print("Machine halted")
+        break
 
     memline = memline + 1
-    print("valueInRs = "+str(valueInRs) + " rtAddr = " +str(rtAddr) +" IMM = " +str(addr))
-    i = 0
-    while i < (len(mem)):
-        print("mem[ " + str(i) +  " ] " + mem[i])
-        i = i + 1
-
-    j = 0
-    while j < (len(reg)):
-        print("reg[ " + str(j) +  " ] " + str(reg[j]))
-        j = j + 1
-
-
-
-
-
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
