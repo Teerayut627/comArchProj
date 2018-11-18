fe14f817ef9a65a9c40f5c1491ae1a67b00226ab
Teerayut627 <31033162+Teerayut627@users.noreply.github.com>
2018-11-18 18:05:35 +0700

    LW with no negative number detection

--- comArchProj.py
+++ comArchProj.py
@@ -42,12 +42,14 @@ with open('D:/CPE/ComArch/test.txt') as f:
     line_label = []
     fill_list = []
     fill_value = []
-    
-
+    machine = ""
+    fi = open("D:/CPE/ComArch/machineCode.txt","w")
+#collect Label(instr[0])    
     for i in lines:
         instr = i.split(' ')
         line_label.append(instr[0])
 
+#collect Label(instr[0]) and fill_value
     for i in lines:
         instr = i.split(' ')
         if(instr[1] == ".fill"):
@@ -58,19 +60,18 @@ with open('D:/CPE/ComArch/test.txt') as f:
     # print(fill_value)
     current_address = 0
     for i in lines:
-        
         #print(i)
-
         instr = i.split(' ')
         #print(len(instr))
-
-        if(instr[1] == "add" or instr[1] == "nand"): #R-Type
+    #R-Type
+        if(instr[1] == "add" or instr[1] == "nand"): 
             x = int(instr[2])
             y = int(instr[3])
             z = int(instr[4])
             print(int(bindigits(0,7) + instuction(instr[1]) + bindigits(x,3) + bindigits(y,3) +bindigits(0,13)+ bindigits(z,3),2)) 
-
-        elif(instr[1] == "lw" or instr[1] == "sw"): #I-Type
+            machine = bindigits(0,7) + instuction(instr[1]) + bindigits(x,3) + bindigits(y,3) +bindigits(0,13)+ bindigits(z,3)
+    #I-Type
+        elif(instr[1] == "lw" or instr[1] == "sw"): 
             x = int(instr[2])
             y = int(instr[3])
             z = instr[4]
@@ -82,13 +83,30 @@ with open('D:/CPE/ComArch/test.txt') as f:
                     break
                 count = count + 1
             if(find == True):
-                z = (count - current_address) 
+                z = (count - current_address)
             print(int(bindigits(0,7) + instuction(instr[1]) +bindigits(x,3) +bindigits(y,3) + bindigits(int(z),16),2))
-
+            machine = bindigits(0,7) + instuction(instr[1]) +bindigits(x,3) +bindigits(y,3) + bindigits(int(z),16)
+       
+    #.fill
         elif(instr[1] == ".fill"):
             x = instr[2]
-            print(int(x))
-
+            find = False
+            count = 0
+            while(count < len(line_label)):
+                if(line_label[count] == instr[2]):
+                    find = True
+                    break
+                count = count + 1
+            if(find == True):
+                z = count
+                print(int(z))
+                machine = bindigits(int(z),32)
+                
+            else:
+                print(int(x))
+                machine = bindigits(int(x),32)
+              
+    #beq
         elif(instr[1] == "beq"):
             x = int(instr[2])
             y = int(instr[3])
@@ -104,11 +122,91 @@ with open('D:/CPE/ComArch/test.txt') as f:
             if(find == True): 
                 z = (count - current_address) - 1
             print(int(bindigits(0,7) + instuction(instr[1]) +bindigits(x,3) +bindigits(y,3) + bindigits(int(z),16),2))
-       
-        elif(instr[1] == "jalr"): #J-Type
+            machine = bindigits(0,7) + instuction(instr[1]) +bindigits(x,3) +bindigits(y,3) + bindigits(int(z),16)
+           
+    #J-Type       
+        elif(instr[1] == "jalr"):
             x = int(instr[2])
             print(int(bindigits(0,7) + instuction(instr[1]) +bindigits(x,3) +bindigits(y,3)+bindigits(0,16),2))
-
+            machine = bindigits(0,7) + instuction(instr[1]) +bindigits(x,3) +bindigits(y,3)+bindigits(0,16)
+           
         elif(instr[1] == "halt" or instr[1] == "noop"): #O-Type
             print(int(bindigits(0,7) + instuction(instr[1]) +bindigits(0,22),2))
+            machine = bindigits(0,7) + instuction(instr[1]) +bindigits(0,22)  
+        
         current_address = current_address + 1 
+        
+        fi.write(machine + " \n")
+    fi.close()  
+
+mem = []
+reg = [0,0,0,0,0,0,0,0]
+with open('D:/CPE/ComArch/machineCode.txt') as f:
+    lines = f.readlines()
+    for i in lines:
+        memory = i.split(' ')
+        mem.append(memory[0])
+
+
+
+memline = 0
+while(memline < 2):
+    opcode = mem[int(memline)][7:10]
+
+    if (opcode == "010"):
+        rs = mem[int(memline)][10:13]
+        rt = mem[int(memline)][13:16]
+        offset = mem[int(memline)][16:32]
+        print("opcode = "+opcode +" "+"rs = " +rs +" "+ "rt = " +rt +" "+ "offset = " +offset)
+        
+        # addr = int(mem[int(offset,2)],2) 
+        addr = int(offset,2)
+        valueInRs = reg[int(rs,2)]
+        rtAddr = int(rt,2)
+        addrValue = addr + int(valueInRs)
+        reg[rtAddr] = int(mem[addrValue],2)
+
+
+    memline = memline + 1
+    print("valueInRs = "+str(valueInRs) + " rtAddr = " +str(rtAddr) +" IMM = " +str(addr))
+    i = 0
+    while i < (len(mem)):
+        print("mem[ " + str(i) +  " ] " + mem[i])
+        i = i + 1
+
+    j = 0
+    while j < (len(reg)):
+        print("reg[ " + str(j) +  " ] " + str(reg[j]))
+        j = j + 1
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
