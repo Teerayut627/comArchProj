88c537278ea709a8baa71fc0735646efdea7c452
Teerayut627 <31033162+Teerayut627@users.noreply.github.com>
2018-11-24 16:27:27 +0700

    Multiplication 32 bits
    last commit is Wrong file! This file is the real fixed file

--- Simulator.py
+++ Simulator.py
@@ -30,26 +30,18 @@ def add_binary_nums(x, y) : #-----Add binary n bit-----
 
 #------------------Read Decimal Machine Code form text------------------
 decimalCode = []
-with open('D:/CPE/ComArch/Multest.txt') as f:
+with open('D:/CPE/ComArch/Multest2.txt') as f:
     lines = f.readlines()
     for i in lines:
         memory = i.split(' ')
         decimalCode.append(memory[0])
-    
-fi = open("D:/CPE/ComArch/MulSimulator.txt","w")
+
 #------------------Select only Machine Code------------------
 memDecimal = []
 for i in decimalCode:
     Soi = i.split('\n')
     memDecimal.append(Soi[0])
-
-example = 0
-fi.write("Run of Simulator")
-fi.write("\n")
-while example < (len(memDecimal)):
-    fi.write("     memory[ " + str(example) +  " ] = " + str(memDecimal[example]) +"\n")
-    example = example + 1 
-fi.write("\n")    
+print(memDecimal)
 
 #------------------Prepare Binarry Machinne Code in Memory for Processing------------------
 mem = []
@@ -62,114 +54,171 @@ while (strLines < len(memDecimal)):
 
 #------------------Processing follow ------------------
 
-memline = 0
+PC = 0
 ExecutedInstr = 1
-while(memline < len(mem)):
-    opcode = mem[int(memline)][7:10]
+while(PC < len(mem)):
+    opcode = mem[int(PC)][7:10]
     #-----Print Simulator Result-----
-    fi.write("@@@" +"\n")
-    fi.write("state :" +"\n")
-    fi.write("     PC : " + str(memline) +"\n")
+    print("@@@")
+    print("state :")
+    print("     PC : " + str(PC))
     #-----Print Memory-----
-    fi.write("     memory :" +"\n")
-    i = 0
-    while i < (len(mem)):
-        if(mem[i][0:1] == "1"):
-            memPrint = int(add_binary_nums(sign_bit(mem[i]),'1'),2)*(-1)
-        else:
-            memPrint = int(mem[i],2)
-        fi.write("     mem[ " + str(i) +  " ] " + str(memPrint) +"\n")
-        i = i + 1
+    print("     memory :")
+    # i = 0
+    # while i < (len(mem)):
+    #     if(mem[i][0:1] == "1"):
+    #         memPrint = int(add_binary_nums(sign_bit(mem[i]),'1'),2)*(-1)
+    #     else:
+    #         memPrint = int(mem[i],2)
+    #     print("     mem[ " + str(i) +  " ] " + str(memPrint))
+    #     i = i + 1
     #-----Print Register-----
-    fi.write("register :" +"\n")
+    print("register :")
     j = 0
     while j < (len(reg)):
-        fi.write("     reg[ " + str(j) +  " ] " + str(reg[j]) +"\n")
+        print("     reg[ " + str(j) +  " ] " + str(reg[j]))
         j = j + 1
-    fi.write("end state" +"\n")
+    print("end state")
     #ADD
     if (opcode == "000"):
-        rs = int(mem[int(memline)][10:13],2) 
-        rt = int(mem[int(memline)][13:16],2) 
-        destReg = int(mem[int(memline)][29:32],2) 
-        addResult = int(reg[rt]) + int(reg[rs]) 
-        reg[destReg] = addResult
+        rs = int(mem[int(PC)][10:13],2) 
+        rt = int(mem[int(PC)][13:16],2) 
+        destReg = int(mem[int(PC)][29:32],2) 
+
+        addABinary = bindigits(reg[rs],33)[-32:] #Limits bits
+        addBBinary = bindigits(reg[rt],33)[-32:]
+
+        if ( int(reg[rs]) < 0):
+            valueA = int(add_binary_nums(sign_bit(addABinary),"1"),2)*(-1)
+        else:
+            valueA = int(addABinary,2)
+
+        if ( int(reg[rt]) < 0):
+            valueB = int(add_binary_nums(sign_bit(addBBinary),"1"),2)*(-1)
+        else:
+            valueB = int(addBBinary,2)
+
+        addResult = valueA + valueB 
+
+        addResultBinary = bindigits(addResult,33)[-32:]
+        if ( addResult < 0):
+            xxxx = int(add_binary_nums(sign_bit(addResultBinary),"1"),2)*(-1)
+        else:
+            xxxx = int(addResultBinary,2)
+
+        reg[destReg] = xxxx
+
+
     #NAND
     if (opcode == "001"):
-        rs = int(mem[int(memline)][10:13],2) 
-        rt = int(mem[int(memline)][13:16],2) 
-        destReg = int(mem[int(memline)][29:32],2) 
-        reg[destReg] = ( ~( int(reg[rt],2) & int(reg[rs],2) ) )  
+        rs = int(mem[int(PC)][10:13],2) 
+        rt = int(mem[int(PC)][13:16],2) 
+        destReg = int(mem[int(PC)][29:32],2) 
+
+        print(bindigits(reg[rs],33)[-32:])
+        print(bindigits(reg[rt],33)[-32:])
+
+        nandABinary = bindigits(reg[rs],33)[-32:]
+        nandBBinary = bindigits(reg[rt],33)[-32:]
+
+        if( reg[rs] < 0 ):
+            nandA = int(add_binary_nums(sign_bit(nandABinary),"1"),2)*(-1)
+        else:
+            nandA = int(nandABinary,2)
+
+        if( reg[rt] < 0  ):
+            nandB = int(add_binary_nums(sign_bit(nandBBinary),"1"),2)*(-1)
+        else:
+            nandB = int(nandBBinary,2)
+        
+
+        reg[destReg] = ~(nandA & nandB)
+
     #LW
     if (opcode == "010"):
-        rs = int(mem[int(memline)][10:13],2) 
-        rt = int(mem[int(memline)][13:16],2) 
-        offset = int(mem[int(memline)][16:32],2) 
-        addrValue = offset + int(reg[rs])
+        rs = int(mem[int(PC)][10:13],2) 
+        rt = int(mem[int(PC)][13:16],2) 
+        offset = int(mem[int(PC)][16:32],2) 
+        addrValue = offset + int(reg[rs]) + PC
         if(mem[addrValue][0:1] == "1"):
             reg[rt] = int(add_binary_nums(sign_bit(mem[addrValue]),'1'),2)*(-1)
         else:
             reg[rt] = int(mem[addrValue],2)
     #SW
     if (opcode == "011"):
-        rs = int(mem[int(memline)][10:13],2) 
-        rt = int(mem[int(memline)][13:16],2) 
-        offset = int(mem[int(memline)][16:32],2) 
+        rs = int(mem[int(PC)][10:13],2) 
+        rt = int(mem[int(PC)][13:16],2) 
+        offset = int(mem[int(PC)][16:32],2) 
         addrValue = offset + int(reg[rs])
         mem[addrValue] = int(reg[rt],2)
     #BEQ
     if (opcode == "100"): 
-        rs = int(mem[int(memline)][10:13],2) 
-        rt = int(mem[int(memline)][13:16],2) 
-        offset = mem[int(memline)][16:32]
-        if( reg[rt] == reg[rs]):
-            if(mem[memline][16:17] == "1"):
+        rs = int(mem[int(PC)][10:13],2) 
+        rt = int(mem[int(PC)][13:16],2) 
+        offset = mem[int(PC)][16:32]
+
+        beqABinary = bindigits(reg[rs],33)[-32:] #Limits bits
+        beqBBinary = bindigits(reg[rt],33)[-32:]
+
+        if ( int(reg[rs]) < 0):
+            valueA = int(add_binary_nums(sign_bit(beqABinary),"1"),2)*(-1)
+        else:
+            valueA = int(beqABinary,2)
+
+        if ( int(reg[rt]) < 0):
+            valueB = int(add_binary_nums(sign_bit(beqBBinary),"1"),2)*(-1)
+        else:
+            valueB = int(beqBBinary,2)
+
+
+        if( valueA == valueB):
+            if(mem[PC][16:17] == "1"):
                 offsetJamp = int(add_binary_nums(sign_bit(offset),"1"),2)*(-1)
             else:
                 offsetJamp = int(offset,2)
-            jampAddr = memline + offsetJamp
-            memline = jampAddr
+            jampAddr = PC + offsetJamp
+            PC = jampAddr
     #JALR
     if (opcode == "101"):
-        rs = int(mem[int(memline)][10:13],2) 
-        rd = int(mem[int(memline)][13:16],2) 
+        rs = int(mem[int(PC)][10:13],2) 
+        rd = int(mem[int(PC)][13:16],2) 
         if(rs == rd): #Have yet to Verify 
-            reg[rt] = memline
-            memline = reg[rs]
+            reg[rt] = PC
+            PC = reg[rs]
         else: #if and else have the same process
-            reg[rt] = memline
-            memline = reg[rs]
+            reg[rt] = PC
+            PC = reg[rs]
     #HALT
     if (opcode == "110"):
-        fi.write("Machine halted" +"\n")
+        print("Machine halted")
         break
     #NOOP
     if (opcode == "111"):
         continue
-    memline = memline + 1
+    PC = PC + 1
     ExecutedInstr = ExecutedInstr + 1
 
 #-----Final Print Simulator Result-----
-fi.write("Total of " +str(ExecutedInstr) +" instructions executed" +"\n")
-fi.write("Final State of Machine :" +"\n")
-fi.write("@@@" +"\n")
-fi.write("state :" +"\n")
-fi.write("     PC : " + str(memline + 1) +"\n")
+print("Total of " +str(ExecutedInstr) +" instructions executed")
+print("Final State of Machine :")
+print("@@@")
+print("state :")
+print("     PC : " + str(PC + 1))
 #-----Print Memory-----
-fi.write("     memory :" +"\n")
+print("     memory :")
 i = 0
 while i < (len(mem)):
     if(mem[i][0:1] == "1"):
         memPrint = int(add_binary_nums(sign_bit(mem[i]),'1'),2)*(-1)
     else:
         memPrint = int(mem[i],2)
-    fi.write("     mem[ " + str(i) +  " ] " + str(memPrint) +"\n")
+    print("     mem[ " + str(i) +  " ] " + str(memPrint))
     i = i + 1
 #-----Print Register-----
-fi.write("register :" +"\n")
+print("register :")
 j = 0
 while j < (len(reg)):
-    fi.write("     reg[ " + str(j) +  " ] " + str(reg[j]) +"\n")
+    print("     reg[ " + str(j) +  " ] " + str(reg[j]))
     j = j + 1 
-fi.write("end state" +"\n")   
+print("end state")   
     
